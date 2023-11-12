56d2d6f08a68c8625ee1336a9aef5313fba1fb96
Jose Arroyave <arquos.14@gmail.com>
2023-11-12 16:40:48 -0500

    refactor: modularize movie aapp

--- main.py
+++ main.py
@@ -1,24 +1,17 @@
 """run application: uvicorn main:app --reload --port 5000"""
 
-from typing import Any, Dict, Coroutine, Optional
-
-from fastapi import FastAPI, Body, Request, HTTPException, Depends, Query
+from fastapi import FastAPI
 from fastapi.responses import HTMLResponse, JSONResponse
 
-from fastapi.security.http import HTTPAuthorizationCredentials
-from fastapi.encoders import jsonable_encoder
+from jwt_manager import create_token
 
-from pydantic import BaseModel, Field
-from starlette.requests import Request
-from jwt_manager import create_token, validate_token
+from config.database import Base, engine
 
-from config.database import Base, engine, Session
-from models.movie import Movie as MovieModel
+from middlewares.error_handler import ErrorHandlrer
+from middlewares.jwt_bearer import JWTBearer
 
-from middleware.error_handler import ErrorHandlrer
-from middleware.jwt_bearer import JWTBearer
+from routers.movie import movie_router
 
-from schemas.movie import Movie
 from schemas.user import User
     
 app = FastAPI()
@@ -28,14 +21,9 @@ app.version = "0.0.1"
 
 app.add_middleware(ErrorHandlrer)
 
+app.include_router(movie_router)
+
 Base.metadata.create_all(bind=engine)
-
-def get_movie_by_id(id : int):
-    movies = None
-    for movie in movies:
-        if movie["id"] == id:
-            return movie
-
 
 @app.post('/login', tags=['auth'])
 def login(user : User) -> User:
@@ -48,90 +36,4 @@ def login(user : User) -> User:
 async def root():
     return HTMLResponse(
         content="<h1>HELLO WORLD!</h1>"
-    )
-
-@app.get("/movies", tags=["movies"])
-def get_movies():
-    db = Session()
-    movies = db.query(MovieModel).all()
-    return JSONResponse(content=jsonable_encoder(movies), status_code=200)
-
-@app.get("/movies/{id}", tags=["movies"])
-def get_movie(id : int):
-   db = Session()
-   movie_model = db.query(MovieModel).filter(MovieModel.id == id).first()
-
-   if not movie_model:
-        return JSONResponse(status_code=404, content={
-            "message": "record not found"
-        })
-   
-   return JSONResponse(status_code=200, content=jsonable_encoder(movie_model))
-
-@app.get("/movies/", tags=["movies"]) # añadir / para que tome el queryparam
-def get_movie_by_category(category : str = Query(min_length=3, max_length=20)): # los argumento son los query params
-    db = Session()
-    movies = db.query(MovieModel).filter(MovieModel.category == category).all()
-    
-    if not movies:
-        return JSONResponse(status_code=404, content={
-            "message": "record not found"
-        })
-    
-    return JSONResponse(status_code=200, content=jsonable_encoder(movies))
-
-@app.post("/movies", tags=["movies"], response_model=Movie, status_code=201)
-def create_movie(movie : Dict[Any, Any]) -> dict:
-    # print('create a new movie')
-    # print(f'input movie: {movie}')
-
-    db = Session()
-    new_movie = MovieModel(**movie)
-    db.add(new_movie)
-    db.commit()
-    return JSONResponse(
-        content={
-            "movie": movie,
-            "message": "movie register succesfully"
-            },
-        status_code=201
-    )
-
-@app.put('/movies/{id}', tags=['movies'], status_code=200, response_model=dict)
-def update_movie(id : int, movie : Dict[Any, Any]):
-    db = Session()
-    movie_record = db.query(MovieModel).filter(MovieModel.id == id).first()
-
-    if not movie_record:
-        return JSONResponse(status_code=400, content={
-            "message": "movie not found, invalid id"
-        })
-    
-    print('movie: ', movie_record)
-    movie_record.title = movie["title"]
-    movie_record.overview = movie["overview"]
-    movie_record.year = movie["year"]
-    movie_record.rating = movie["rating"]
-    movie_record.category = movie["category"]
-    db.commit()
-    return JSONResponse(status_code=200, content={
-        "message": "movie update successfully"
-    })    
-
-@app.delete('/movies/{id}', tags=['movies'], status_code=200)
-def remove_movie(id : int):
-    db = Session()
-    movie = db.query(MovieModel).filter(MovieModel.id == id).first()
-    if not movie:
-        return JSONResponse(status_code=400, content={
-            "message": "movie not found, invalid id"
-        })
-    
-    db.delete(movie)
-    db.commit()
-    return JSONResponse(
-        status_code=200,
-        content={
-            "message": "movie deleted succesfully!"
-        }
     )
---
+++ routers/movie.py
@@ -0,0 +1,100 @@
+from typing import Any, Dict
+
+from fastapi import Query
+from fastapi import APIRouter
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse
+
+from config.database import Session
+
+from models.movie import Movie as MovieModel
+
+from schemas.movie import Movie
+
+movie_router = APIRouter()
+
+@movie_router.get("/movies", tags=["movies"])
+def get_movies():
+    db = Session()
+    movies = db.query(MovieModel).all()
+    return JSONResponse(content=jsonable_encoder(movies), status_code=200)
+
+@movie_router.get("/movies/{id}", tags=["movies"])
+def get_movie(id : int):
+   db = Session()
+   movie_model = db.query(MovieModel).filter(MovieModel.id == id).first()
+
+   if not movie_model:
+        return JSONResponse(status_code=404, content={
+            "message": "record not found"
+        })
+   
+   return JSONResponse(status_code=200, content=jsonable_encoder(movie_model))
+
+@movie_router.get("/movies/", tags=["movies"]) # añadir / para que tome el queryparam
+def get_movie_by_category(category : str = Query(min_length=3, max_length=20)): # los argumento son los query params
+    db = Session()
+    movies = db.query(MovieModel).filter(MovieModel.category == category).all()
+    
+    if not movies:
+        return JSONResponse(status_code=404, content={
+            "message": "record not found"
+        })
+    
+    return JSONResponse(status_code=200, content=jsonable_encoder(movies))
+
+@movie_router.post("/movies", tags=["movies"], response_model=Movie, status_code=201)
+def create_movie(movie : Dict[Any, Any]) -> dict:
+    # print('create a new movie')
+    # print(f'input movie: {movie}')
+
+    db = Session()
+    new_movie = MovieModel(**movie)
+    db.add(new_movie)
+    db.commit()
+    return JSONResponse(
+        content={
+            "movie": movie,
+            "message": "movie register succesfully"
+            },
+        status_code=201
+    )
+
+@movie_router.put('/movies/{id}', tags=['movies'], status_code=200, response_model=dict)
+def update_movie(id : int, movie : Dict[Any, Any]):
+    db = Session()
+    movie_record = db.query(MovieModel).filter(MovieModel.id == id).first()
+
+    if not movie_record:
+        return JSONResponse(status_code=400, content={
+            "message": "movie not found, invalid id"
+        })
+    
+    print('movie: ', movie_record)
+    movie_record.title = movie["title"]
+    movie_record.overview = movie["overview"]
+    movie_record.year = movie["year"]
+    movie_record.rating = movie["rating"]
+    movie_record.category = movie["category"]
+    db.commit()
+    return JSONResponse(status_code=200, content={
+        "message": "movie update successfully"
+    })    
+
+@movie_router.delete('/movies/{id}', tags=['movies'], status_code=200)
+def remove_movie(id : int):
+    db = Session()
+    movie = db.query(MovieModel).filter(MovieModel.id == id).first()
+    if not movie:
+        return JSONResponse(status_code=400, content={
+            "message": "movie not found, invalid id"
+        })
+    
+    db.delete(movie)
+    db.commit()
+    return JSONResponse(
+        status_code=200,
+        content={
+            "message": "movie deleted succesfully!"
+        }
+    )

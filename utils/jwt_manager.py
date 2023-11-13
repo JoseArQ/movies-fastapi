524e3ccf8625c9c6260af743cde127217d680aca
Jose Arroyave <arquos.14@gmail.com>
2023-11-13 12:52:01 -0500

    feat: implemente token auth

---
+++ utils/jwt_manager.py
@@ -0,0 +1,17 @@
+from jwt import encode, decode
+
+def create_token(data : dict):
+    token : str = encode(
+        payload=data, 
+        key='secrete_key',
+        algorithm='HS256'
+        )
+    return token
+
+def validate_token(token : str):
+    data : dict = decode(
+        token,
+        key='secrete_key',
+        algorithms=['HS256']
+    )
+    return data

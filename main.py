6817be5024b5d6d229c1b8abfe7b5c53fa028e20
Jose Arroyave <arquos.14@gmail.com>
2023-11-13 12:06:53 -0500

    refactor: separate user api in router

--- main.py
+++ main.py
@@ -3,17 +3,14 @@
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse, JSONResponse
 
-from jwt_manager import create_token
-
 from config.database import Base, engine
 
 from middlewares.error_handler import ErrorHandlrer
 from middlewares.jwt_bearer import JWTBearer
 
 from routers.movie import movie_router
+from routers.user import user_router
 
-from schemas.user import User
-    
 app = FastAPI()
 
 app.title = "My first FastApi application"
@@ -22,15 +19,10 @@ app.version = "0.0.1"
 app.add_middleware(ErrorHandlrer)
 
 app.include_router(movie_router)
+app.include_router(user_router)
 
 Base.metadata.create_all(bind=engine)
 
-@app.post('/login', tags=['auth'])
-def login(user : User) -> User:
-    token : str = ''
-    if user.email == "admin@email.com" and user.password == "admin":
-        token  = create_token(data=user.dict())
-    return JSONResponse(content=token, status_code=200)
   
 @app.get("/", tags=["home"])
 async def root():
